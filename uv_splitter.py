f16db39764f4554731583b7e64abb778e1e2c2fe
Jan <grimesarisao@gmail.com>
2018-10-16 21:56:28 +0300

    Update uv_splitter.py

--- uv_splitter.py
+++ uv_splitter.py
@@ -35,7 +35,7 @@ class SplitUV(bpy.types.Operator):
 
     @classmethod
     def poll(cls, context):
-        return bpy.context.object.mode == 'OBJECT'
+        return bpy.context.mode == 'OBJECT'
 
     def split(self, uv_size, obj, cicled):
         bpy.ops.shotariya.uv_fixer()
