dbb4ad92d0ec92758deee8998279c81eb6a74782
Grim-es <grimesarisao@gmail.com>
2018-12-08 20:37:10 +0200

    Installation fix

--- __init__.py
+++ __init__.py
@@ -25,7 +25,7 @@ bl_info = {
     'name': "Shotariya's Material Combiner",
     'description': 'Public Release Material Combiner 2',
     'author': 'shotariya',
-    'version': (2, 0, 3, 1),
+    'version': (2, 0, 3, 2),
     'blender': (2, 79, 0),
     'location': 'View3D',
     # 'warning': '',
